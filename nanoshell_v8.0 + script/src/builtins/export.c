31ea3691214a89582cdf35f226ba127167a26f8c
J4V13R <sujaa92@gmail.com>
2025-12-29 10:39:34 +0100

    salida errores + script

---
+++ nanoshell_v8.0 + script/src/builtins/export.c
@@ -0,0 +1,168 @@
+
+#include "nanoshell.h"
+
+int is_valid_identifier(char *arg)
+{
+    int i;
+
+    if (!arg || arg[0] == '\0')
+        return (0);
+    if (!ft_isalpha(arg[0]) && arg[0] != '_')
+        return (0);
+    i = 1;
+    while (arg[i])
+    {
+        if ( !ft_isalnum(arg[i]) && arg[i] != '_')
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+// hace el set de *name y *value pero no los retorna
+void    split_name_value(const char *arg, char **name, char **value)
+{
+    char    *equal;
+
+    if (!arg || !name || !value)
+        return ;
+    equal = ft_strchr(arg, '=');
+    if (!equal)
+    {
+        *name = ft_strdup(arg);
+        if (!*name)
+        {
+            *value = NULL;
+            return;
+        }
+        *value = NULL;
+        return ;
+    }
+    *name = ft_substr(arg, 0, equal - arg);
+    if (!*name)
+    {
+        *value = NULL;
+        return;
+    }
+    *value = ft_strdup(equal + 1);
+    if (!*value)
+    {
+        free(*name);
+        *name = NULL;
+        return ;
+    }
+}
+
+void    print_sorted_env(char **envp)
+{
+    char    **tmp;
+    char    *name;
+    char    *value;
+    int     count;
+    int     i;
+    int     j;
+
+    // count entries
+    count = 0;
+    while (envp[count])
+        count++;
+    // duplicate envp
+    tmp = dup_env(envp);
+    // sort tmp (buble sort)    
+    i = 0;
+    while (i < count)
+    {
+        j = i + 1;
+        while (j < count)
+        {
+            if (ft_strcmp(tmp[i], tmp[j]) > 0)
+            {
+                ft_swap_str(&tmp[i], &tmp[j]);
+            }
+            j++;
+        }
+        i++;
+    }
+    // Print each entry in declare -x format
+    i = 0;
+    while (i < count)
+    {
+        name = NULL;
+        value = NULL;
+        split_name_value(tmp[i], &name, &value);
+        if (!name)
+        {
+            i++;
+            continue ;
+        }
+        if (!value)
+            printf("declare -x %s\n", name);
+        else
+            printf("declare -x %s=\"%s\"\n", name, value);
+        free(name);
+        free(value);
+        i++;
+    }
+    free_env(tmp);
+}
+
+int builtin_export(char **argv, t_data *data)
+{
+    int     i;
+    int     status;
+    char    *name;
+    char    *value;
+
+    name = NULL;
+    value = NULL;
+    status = 0;
+    // argv[0] == export 
+    // case of export with no arguments
+    if (argv[1] == NULL)
+    {
+        print_sorted_env(data->envp);
+        data->last_status = 0;
+        return (0);
+    }
+    i = 1;
+    while (argv[i])
+    {
+        split_name_value(argv[i], &name, &value);
+        if (!name)
+        {
+            status = 1;
+            data->last_status = 1;
+            i++;
+            continue ;
+        }
+        if (!is_valid_identifier(name))
+        {
+            builtin_error("export", argv[i], "not a vlaid identifier");
+            status = 1;
+        }
+        else
+        {
+            if (value == NULL)
+                set_env_var_data(data, name , ""); // export NAME
+            else
+                set_env_var_data(data, name, value); // export NAME=VALUE
+        }
+        free(name);
+        free(value);
+        i++;
+    }
+    data->last_status = status;
+    return (status);
+}
+
+
+/*
+function print_sorted_declare_x(envp):
+    # 1) count n
+    # 2) duplicate envp -> tmp
+    # 3) bubble sort tmp with strcmp
+    # 4) for each tmp[i]:
+    #       split at '='
+    #       print "declare -x NAME=\"VALUE\""
+    # 5) free tmp strings + array
+*/
---
+++ nanoshell_v8.0 + script/src/executor/exec_utils.c
@@ -0,0 +1,82 @@
+
+#include "nanoshell.h"
+
+void	exec_error(const char *message, const char *arg)
+{
+	write(2, "Executor: ", 10);
+	write(2, message, ft_strlen_const(message));
+	if (arg)
+	{
+		write(2, ":", 1);
+		write(2, arg, ft_strlen_const(arg));
+	}
+	write(2, "\n", 1);
+}
+
+char *heredoc_tmp_name(void)
+{
+	static int	n;
+	char		*num;
+	char		*name;
+
+	if (n < 0)
+		n = 0;
+	num = ft_itoa(n);
+	if (!num)
+		return (NULL);
+	n++;
+	name = ft_strjoin(".heredoc_", num);
+	free(num);
+	return (name);
+}
+
+char *create_heredoc_tmp(char *delimeter)
+{
+	char	*path;
+	char	*line;
+	int		fd;
+	size_t	len_line;
+	size_t	len_delim;
+
+	path = heredoc_tmp_name();
+	if (!path)
+		return (NULL);
+	// only your minishell user can read/write the file
+	fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+	{
+		exec_error("open failed", path);
+		free(path);
+		return (NULL);
+	}
+	while (1)
+	{
+		line = readline("> ");
+		if (!line)
+		{
+			if (g_signal_received == SIGINT)
+        	{
+            	// heredoc cancelado por Ctrl+C
+            	close(fd);
+            	unlink(path);      // borrar tmp
+            	free(path);
+            	g_signal_received = 0; // opcional, para no arrastrar el estado
+            	return (NULL);     // indicar al parser que se canceló
+        	}
+			break; // EOF normal
+		}
+			
+		len_line = ft_strlen(line);
+		len_delim = ft_strlen(delimeter);
+		if (len_line == len_delim && ft_strncmp(line, delimeter, len_delim) == 0)
+		{
+			free(line);
+			break; // reached delimeter
+		}
+		write (fd, line, ft_strlen(line));
+		write (fd, "\n", 1);
+		free(line);
+	}
+	close(fd);
+	return (path); 
+}
